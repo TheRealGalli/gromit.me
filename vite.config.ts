77e76706aa4891bcd6575b6d7f86a5e1dfadba26
Carlo Galli <carlogalli@MacBookAir.home>
2026-05-15 21:22:02 +0200

    Update base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
-  base: "/gromit.me/",
+  base: "/",
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
