4620dd3902596b28175c2d5bda8e135256299d0c
Carlo Galli <carlogalli@MacBookAir.home>
2026-05-15 21:18:32 +0200

    Configure GitHub Pages deployment via Actions

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
+  base: "/gromit.me/",
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
